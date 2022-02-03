69a3cfb20631f973b732b9f0d14a2589bf29d2d5
ericmtodd <eterictodd@gmail.com>
2022-02-02 17:07:24 -0700

    Cities are now randomized every fifteen minutes if location is not shared.

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,45 +4,100 @@ import axios from 'axios'
 const Weather = () => {
   const [celsius, setCelsius] = useState(0)
   const [fahrenheit, setFahrenheit] = useState(0)
-  const [kelvin, setKelvin] = useState(0)
   const [icon, setIcon] = useState('')
-  const [city, setCity] = useState('London')
+  const cities = [
+    'Tokyo',
+    'London',
+    'New York',
+    'Dublin',
+    'Beijing',
+    'Moscow',
+    'Los Angeles',
+    'Denver',
+    'Salt Lake City',
+    'Atlanta',
+    'Washington D.C.',
+    'Berlin',
+    'Edinburgh',
+    'Dehli',
+    'Mumbai',
+    'Baghdad',
+    'Johannesburg',
+    'Mexico City',
+    'Sao Paulo',
+    'Guadalahara',
+    'Lima',
+    'Bangkok',
+    'Hong Kong',
+    'Paris',
+    'Vienna',
+    'Reykjavik',
+    'Oslo',
+    'Bern',
+    'Brussels'
+  ]
+  const [city, setCity] = useState(cities[Math.floor(Math.random() * cities.length)])
+  const [country, setCountry] = useState('')
   const apiKey = '8d9b598296f463cb05d7baea7c741c65'
 
-  useEffect(() => {
-    if ("geolocation" in navigator) {
-      // console.log("Available")
+  const getRandomCity = () => {
+    let newCity = cities[Math.floor(Math.random() * cities.length)]
+    setCity(newCity)
 
-      navigator.geolocation.getCurrentPosition((position) => {
-        axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}`)
-        .then(response => {
-          console.log(response.data)
-          setKelvin(response.data.main.feels_like)
-          setCelsius(Math.floor(response.data.main.feels_like - 273.15))
-          setFahrenheit(Math.floor((response.data.main.feels_like - 273.15) * 9 / 5 + 32))
-          setIcon(response.data.weather[0].icon)
-          setCity(response.data.name)
-        })
-        .catch(error => console.log(error))
-      });
-    } else {
-      // console.log("Not Available")
-  
-      axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${newCity}&appid=${apiKey}`)
+    .then(response => {
+      setCountry(response.data.sys.country)
+      setCelsius(Math.floor(response.data.main.feels_like - 273.15))
+      setFahrenheit(Math.floor((response.data.main.feels_like - 273.15) * 9 / 5 + 32))
+      setIcon(response.data.weather[0].icon)
+    })
+    .catch(error => console.log(error))
+  }
+
+  const getLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}`)
       .then(response => {
-        setKelvin(response.data.main.feels_like)
+        setCountry(response.data.sys.country)
         setCelsius(Math.floor(response.data.main.feels_like - 273.15))
         setFahrenheit(Math.floor((response.data.main.feels_like - 273.15) * 9 / 5 + 32))
         setIcon(response.data.weather[0].icon)
+        setCity(response.data.name)
       })
       .catch(error => console.log(error))
+    });
+  }
+
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      getLocation()
+      let timer = setInterval(() => {
+        getLocation()
+      }, 900000)
     }
+
+    getRandomCity()
+    let timer = setInterval(() => {
+      getRandomCity()
+    }, 900000)
+    return () => clearInterval(timer)
   }, [])
+
+
+  if (country === 'US') {
+    return(
+      <div id='weather'>
+        <h1>{city}</h1>
+        <h1>{fahrenheit}° F</h1>
+        <img src={`http://openweathermap.org/img/wn/${icon}.png`}/>
+      </div>
+    )
+  }
 
   return(
     <div id='weather'>
       <h1>{city}</h1>
-      <h1>{fahrenheit}° F</h1>
+      <h1>{celsius}° C</h1>
       <img src={`http://openweathermap.org/img/wn/${icon}.png`}/>
     </div>
   )
